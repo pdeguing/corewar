eb1bc335d1ff4d538946d52bbc6553ca433ba848
Pierre-quentin Deguingand <pierrequentindeguingand@gmail.com>
2019-05-25 13:06:26 -0700

    Added documentation and declaration for each function

--- src/assembler/assembler.c
+++ src/assembler/assembler.c
@@ -8,16 +8,19 @@
 
 #include "assembler.h"
 
-t_error		verifyExtension(char *name)
+/*
+** Verifies that the filename is valid with .s extension.
+*/
+t_error		verifyFilename(char *filename)
 {
 	int	len;
 
-	len = strlen(name);
+	len = strlen(filename);
 	if (len <= 3)
 	{
 		return strdup("file name too short");
 	}
-	if (name[len - 1] != 's' || name[len - 2] != '.')
+	if (filename[len - 1] != 's' || filename[len - 2] != '.')
 	{
 		return strdup("incorrect file extension");
 	}
@@ -25,23 +28,46 @@ t_error		verifyExtension(char *name)
 }
 
 /*
-** Parses the file into a t_champ struct, does various verifications of input
-** and returns err in case of error.
+** Reads first line of file described by fd and feeds result into dst.
 */
-t_error		parser(t_champ *champ, char *name)
+t_error		getName(char **dst, int fd)
 {
-	int	fd;
+	t_error		err;
 
-	err = verifyExtension(name)
-	if (err)
-	{
-		return err;	
-	}
-	fd = open(name, O_RDONLY);
-	if (fd < 0)
-	{
-		return strerror(errno);
-	}
+	(void)dst;
+	(void)fd;
+}
+
+/*
+** Reads second line of file described by fd and feeds result into dst.
+*/
+t_error		getComment(char **dst, int fd)
+{
+	t_error		err;
+
+	(void)dst;
+	(void)fd;
+}
+
+/*
+** Reads file line per line until EOF and feeds concatenation of all into
+** dst.
+*/
+t_error		getContent(char **dst, int fd)
+{
+	t_error		err;
+
+	(void)dst;
+	(void)fd;
+}
+
+/*
+** Fills the champ struct with the content of file described by fd.
+*/
+t_error		fillChamp(t_champ *champ, int fd)
+{
+	t_error		err;
+
 	err = getName(&champ->name, fd);
 	if (err)
 	{
@@ -61,6 +87,32 @@ t_error		parser(t_champ *champ, char *name)
 }
 
 /*
+** Parses the file into a t_champ struct, does various verifications of input
+** and returns err in case of error.
+*/
+t_error		parser(t_champ *champ, char *filename)
+{
+	int	fd;
+
+	err = verifyFilename(filename)
+	if (err)
+	{
+		return err;	
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		return strerror(errno);
+	}
+	err = fillChamp(champ, fd);
+	if (err)
+	{
+		return err;
+	}
+	return NULL;
+}
+
+/*
 ** Transforms the t_champ struct into a list of instructions, verifies
 ** that instructions are valid and returns err if they are not.
 */
@@ -70,6 +122,9 @@ t_error		lexer(t_champ *champ)
 	return NULL;
 }
 
+/*
+** Sets all members of t_champ struct to NULL.
+*/
 void		initChamp(t_champ *champ)
 {
 	champ->name = NULL;
@@ -77,13 +132,17 @@ void		initChamp(t_champ *champ)
 	champ->content = NULL;
 }
 
-t_error		assembler(char *name)
+/*
+** Translates assembly source file into a binary file for Corewar virtual
+** machine.
+*/
+t_error		assembler(char *filename)
 {
 	t_error	err;
 	t_champ	champ;
 
 	initChamp(&champ);
-	err = parser(&champ, name);
+	err = parser(&champ, filename);
 	if (err)
 	{
 		return err;
