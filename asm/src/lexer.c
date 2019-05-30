8463ed0f0203030c503ca1b94df5d1afed9997c1
Pierre-Quentin Deguingand <pierrequentindeguingand@gmail.com>
2019-05-30 15:49:42 -0700

    Added structure for compilation

--- asm/src/lexer.c
+++ asm/src/lexer.c
@@ -16,6 +16,22 @@
 */
 t_error		lexer(t_instruction **instructions, t_champ *champ)
 {
-	// 
+	// split champ->content into array of lines
+	// for each line, split by space:
+	//
+	// 			if label:
+	// 				append label to label_array,
+	// 					with offset = total_instruction_size before label
+	// 			if instructions:
+	// 				create instruction struct
+	// 				determine instruction size
+	// 				add instruction size to total_instruction_size
+	// 				append instruction to instruction_array
+	// 					check format
+	// 			if #:
+	// 				ignore rest
+	// 			else if anything else
+	// 				format error
+	//
 	return NULL;
 }
