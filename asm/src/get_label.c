088abb64cfe7eefacb9bd888698bb1a4a8524711
Pierre-Quentin Deguingand <pierrequentindeguingand@gmail.com>
2019-06-01 09:47:53 -0700

    Added format verification

--- asm/src/get_label.c
+++ asm/src/get_label.c
@@ -1,6 +1,20 @@
 #include <assembler.h>
 
 /*
+** Checks if the label is properly formated with only LABEL_CHARS.
+*/
+static int	is_invalid_label(char *label)
+{
+	while (*label)
+	{
+		if (!ft_strchr(LABEL_CHARS, *label))
+			return 1;
+		label++;
+	}
+	return 0;
+}
+
+/*
 ** Gets label from first item of elem by taking what comes before LABEL_CHAR.
 ** Verifies that label if properly formatted with LABEL_CHARS.
 ** Allocate label struct, fill it with name and offset and assign it
@@ -11,16 +25,23 @@ t_error		get_label(t_label **new_label, char **elem)
 	char	*label_char;
 	size_t	len;
 	t_label	*new;
+	char	*name;
 
 	printf(RED"get_label:\n"RESET);
 	label_char = ft_strchr(elem[0], LABEL_CHAR);
 	if (!label_char)
 		return NULL;
+	len = label_char - elem[0];
+	name = ft_strsub(elem[0], 0, len);
+	if (is_invalid_label(name))
+	{
+		free(name);
+		return ft_strjoin("label name is invalid: ", name);
+	}
 	new = malloc(sizeof(t_label));
 	if (!new)
 		return ft_strdup("could not allocate t_label");
-	len = label_char - elem[0];
-	new->name = ft_strsub(elem[0], 0, len);
+	new->name = name;
 	new->offset = g_offset;
 	//printf("new->name = %s / new->offset = %zu\n", new->name, new->offset);
 	*new_label = new;
