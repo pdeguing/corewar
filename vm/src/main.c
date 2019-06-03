4f9225c1bf2fd182b4ad81e29e2d256ff26359bd
Pierre-Quentin Deguingand <pierrequentindeguingand@gmail.com>
2019-06-03 15:34:31 -0700

    Started to work on parsing

--- vm/src/main.c
+++ vm/src/main.c
@@ -2,7 +2,13 @@
 
 int	main(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
+	t_vm	vm;
+
+	ft_bzero(&vm, sizeof(vm));
+	if (parse_args(&vm, ac - 1, av + 1) < 0)
+	{
+		ft_printf("usage: ./corewar  [-dump nbr_cycles] [[-n number] champion1.cor] ...\n");
+		return -1;
+	}
 	return 0;
 }
